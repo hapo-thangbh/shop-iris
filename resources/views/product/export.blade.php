39347730b303e0221993cfc8cbf2d83f2010b9d1
Thang <thangbh@haposoft.com>
2019-10-24 00:15:15 +0700

    fix bug

--- resources/views/product/export.blade.php
+++ resources/views/product/export.blade.php
@@ -128,7 +128,7 @@
                         <div class="col-lg-8">
                             <select class="form-control" id="" required name="status_id">
                                 <option value="" class="d-none" disabled selected>Chọn trạng thái..</option>
-                                @foreach($statuses->orderBy('name') as $status)
+                                @foreach($statuses->sortBy('name') as $status)
                                     <option value="{{ $status->id }}">{{ $status->name }}</option>
                                 @endforeach
                             </select>
